00897d1dcb762d0750463cce425ccadd23f5e7bf
osman yaycioglu <osman.yaycioglu@gmail.com>
2024-09-03 12:13:53 +0300

    day 2 2

---
+++ ms-order/src/main/java/org/training/turkcell/order/MsOrderApplication.java
@@ -0,0 +1,14 @@
+package org.training.turkcell.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsOrderApplication.class,
+                              args);
+    }
+
+}
