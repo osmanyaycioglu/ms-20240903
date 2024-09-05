77b80ae06025901247c3c297f45a793bffc4c429
osman yaycioglu <osman.yaycioglu@gmail.com>
2024-09-05 16:16:52 +0300

    day 4 3

---
+++ ms-pay-api/src/main/java/org/training/turkcell/payment/MsPayApiApplication.java
@@ -0,0 +1,14 @@
+package org.training.turkcell.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPayApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPayApiApplication.class,
+                              args);
+    }
+
+}
